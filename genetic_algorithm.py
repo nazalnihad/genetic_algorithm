3e499dc072de4ca35eaa3fdb3453edb4d4e7846d
Nazal-nihad <nazalnihad17@gmail.com>
2024-03-23 01:22:22 +0530

    example update

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,59 +1,48 @@
 from random import choices, randint, randrange, random
-from typing import List, Callable, Tuple, Optional
 
-Genome = List[int]
-Population = List[Genome]
-FitnessFunc = Callable[[Genome], int]
-PopulateFunc = Callable[[], Population]
-SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
-CrossoverFunc = Callable[[Genome, Genome], Tuple[Genome, Genome]]
-MutationFunc = Callable[[Genome], Genome]
-PrinterFunc = Callable[[Population, int, FitnessFunc], None]
+def generate_genome(length):
+    return choices([0,1], k=length)
 
-def generate_genome(length :int):
-    return choices([0,1],k=length) #returns random 0,1 values with length k
-
-def generate_population(size :int,genome_length :int):
+def generate_population(size, genome_length):
     return [generate_genome(genome_length) for _ in range(size)]
 
-def single_point_crossover(a:Genome , b:Genome):
-    if len(a)!=len(b):
+def single_point_crossover(a, b):
+    if len(a) != len(b):
         raise ValueError("genome length are not same")
 
     length = len(a)
-    if length < 2:
-        return a,b # meaningless to crossover with lenth < 2
+    if length < 2: #splits only if genome lenght > 2
+        return a, b
     
-    partition = randint(1,length-1)
-    # take random partition and split and join to make new genome
-    return a[0:partition]+b[partition:] , b[0:partition]+a[partition:]
+    partition = randint(1, length-1) 
+    # partition and crossover b/w genomes
+    return a[0:partition] + b[partition:], b[0:partition] + a[partition:]
 
-def mutation(genom : Genome , iter :int =1 ,probability:float=0.5):
+def mutation(genom, iter=1, probability=0.5):
     for _ in range(iter):
         index = randrange(len(genom))
-        # flips the 0/1 value by comparing random value generated
-        genom[index] = genom[index] if random()>probability else abs(genom[index]-1)
+        # mutate if probability > random generated probability i.e flips 1/0
+        genom[index] = genom[index] if random() > probability else abs(genom[index] - 1)
     return genom
 
-def population_fitness(population:Population,fitness_func:FitnessFunc):
+def population_fitness(population, fitness_func):
     return sum([fitness_func(genome) for genome in population])
-    # return values of population
 
-def select_pairs(population:Population,fitness_func:FitnessFunc):
-    # returns pair of two best fit according to its weights
+def select_pairs(population, fitness_func):
+    # select 2 best pairs
     return choices(
         population=population,
         weights=[fitness_func(gene) for gene in population],
         k=2
     )
 
-def sort_population(population:Population,fitness_func:FitnessFunc):
-    return sorted(population,key=fitness_func,reverse=True)
+def sort_population(population, fitness_func):
+    return sorted(population, key=fitness_func, reverse=True)
 
-def genome_to_string(genome: Genome) :
+def genome_to_string(genome):
     return "".join(map(str, genome))
 
-def print_stats(population: Population, generation_id: int, fitness_func: FitnessFunc):
+def print_stats(population, generation_id, fitness_func):
     print("GENERATION %02d" % generation_id)
     print("=============")
     print("Population: [%s]" % ", ".join([genome_to_string(gene) for gene in population]))
@@ -67,42 +56,37 @@ def print_stats(population: Population, generation_id: int, fitness_func: Fitnes
 
     return sorted_population[0]
 
-def run_evolution(populate_func:PopulateFunc,
-                  fitness_func:FitnessFunc,
-                  fitness_limit:int,
-                  selection_func:SelectionFunc=select_pairs,
-                  crossover_func:CrossoverFunc=single_point_crossover,
-                  mutation_func:MutationFunc=mutation,
-                  generation_limit:int=100,
-                  printer: Optional[PrinterFunc] = None,
-                  ):
+def run_evolution(populate_func, fitness_func, fitness_limit, selection_func=select_pairs,
+                  crossover_func=single_point_crossover, mutation_func=mutation,
+                  generation_limit=100, printer=None):
+    # random genomes made
     population = populate_func()
 
     for i in range(generation_limit):
-        population = sorted(population,key=lambda genome:fitness_func(genome),reverse=True)
+        population = sorted(population, key=lambda genome: fitness_func(genome), reverse=True)
 
         if printer is not None:
-            printer(population,i,fitness_func)
-        
+            printer(population, i, fitness_func)
+
         if fitness_func(population[0]) >= fitness_limit:
             break
 
         next_gen = population[0:2]
 
         for j in range(int(len(population)/2)-1):
-            parents = selection_func(population,fitness_func)
-            offspring_a,offspring_b = crossover_func(parents[0],parents[1])
+            parents = selection_func(population, fitness_func)
+            offspring_a, offspring_b = crossover_func(parents[0], parents[1])
             offspring_a = mutation_func(offspring_a)
             offspring_b = mutation_func(offspring_b)
-            next_gen += [offspring_a,offspring_b]
-        
+            next_gen += [offspring_a, offspring_b]
+
         population = next_gen
-    return population,i
+    return population, i
 
 
-# Example usage:
-def example_fitness_function(genome: Genome) -> int:
-    # Example fitness function that counts the number of ones in the genome
+
+# Example 
+def example_fitness_function(genome):
     return sum(genome)
 
 if __name__ == "__main__":
